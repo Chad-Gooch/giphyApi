2580c94914faaa7a4e5f4afda04ef1398fabea32
Chad-Gooch <chad.t.gooch88@gmail.com>
2021-11-02 10:22:00 -0400

    first commit

---
+++ assets/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEQoCXEltfuviLmlVR6mCJG-JNUUNDgQk",
+  authDomain: "giphy-api-55b85.firebaseapp.com",
+  projectId: "giphy-api-55b85",
+  storageBucket: "giphy-api-55b85.appspot.com",
+  messagingSenderId: "588417366870",
+  appId: "1:588417366870:web:cbf8d9f084415ee60574f7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+var apiConfig = {
+    key:'ZHI2cEaLFtRFz77I8b0VqK1PW1lxWWSa'
+}
---
+++ script.js
@@ -0,0 +1,3 @@
+const apiKey = 'ZHI2cEaLFtRFz77I8b0VqK1PW1lxWWSa';
+
+console.log(apiKey);
